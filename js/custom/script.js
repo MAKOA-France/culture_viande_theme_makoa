2d854418e1fd1df8471837d60018ef1058a5fc56
sitraka <sitraka@makoa.fr>
2023-07-19 08:07:39 +0200

    page dernier document (stylisation)

--- js/custom/script.js
+++ js/custom/script.js
@@ -3,7 +3,7 @@
   // This is a function to open sidenav menu
     $('body').on('click', '.menu-open', function(){
       $('#main-menu.top-bar').css('width', '200px');
-      $('.content-menu-burger .menu-open').hide();
+      $('.content-menu-burger .menu-open, .content-menu-burger .content-sub-menu-burger').hide();
       $('#main-menu .grid-container nav, .content-menu-burger .menu-close').show();
 
     });
@@ -11,7 +11,7 @@
   // This is a function to close sidenav menu
   $('body').on('click', '.menu-close', function(){
       $('#main-menu.top-bar').css('width', '70px');
-      $('.content-menu-burger .menu-open').show();
+      $('.content-menu-burger .menu-open, .content-menu-burger .content-sub-menu-burger').show();
       $('#main-menu .grid-container nav, .content-menu-burger .menu-close').hide();
 
     });
@@ -28,8 +28,25 @@
     
     $target.toggleClass("d-block");
   });
+  //this is a function dropdown on sidebar for reunion, community, poser question
+  $('body').on('mouseenter mouseleave', '.content-sub-menu-burger .item-sub-menu-burger[data-target]', function(event) {
+    var $target = $($(this).data("target"));
+    var isMouseEnter = event.type === 'mouseenter';
+    $target.toggleClass("d-block", isMouseEnter);
+  });
 
-  //change backtotop
+  //this function is toogle of last document
+  $('body').on('click', '.line-container-plus .btn-see-other-doc, .line-container-moins .btn-dismiss-other-doc', function() {
+    if ($(this).hasClass('btn-see-other-doc')) {
+        $('.section-others-documents').show();
+        $('.line-container-plus').hide();
+    } else {
+        $('.section-others-documents').hide();
+        $('.line-container-plus').show();
+    }
+  });
+  
+  //change backtotop icon
   $('button#backtotop').html('<i class="fa fa-angle-double-up"></i>');
   
 
